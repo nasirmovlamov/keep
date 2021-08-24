fbb55e9f538aa0bd775f9e0605eb5d0edb2013af
nasirmovlamov <movlamovnasir2000@gmail.com>
2021-08-24 22:22:15 +0400

    error handling for build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,15 @@ import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 import '../styles/global/scss/global.css'
 import { Provider } from 'react-redux'
-import store from '../app/store/store'
+import {store} from '../app/store/store'
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    // <Provider> 
-      <Layout store={store}>
+    <Provider store={store}> 
+      <Layout >
           <Component {...pageProps} />
       </Layout>
-    // </Provider>
+    </Provider>
   )
 }
 export default MyApp

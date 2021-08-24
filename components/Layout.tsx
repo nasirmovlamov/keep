a7e514928e4c4ac386a738ef7119d8aa813d439a
nasirmovlamov <movlamovnasir2000@gmail.com>
2021-08-24 18:27:08 +0400

    layout.tsx has updated

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,20 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, FC } from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
 
 
-function Layout({children}): ReactElement {
-    return (
+
+interface Props {
+    // any props that come into the component
+}
+
+const Layout: FC<Props> = ({ children, ...props }) => (
         <>
             <Navbar/>
             <main>{children}</main>   
             <Footer/>   
         </>
-    )
-}
+);
+
 
 export default Layout

c7bc2c5a574ee1fbfff8b3cb4f33413624ad8cba
nasirmovlamov <movlamovnasir2000@gmail.com>
2021-08-24 21:35:18 +0400

    Redux configuering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 import '../styles/global/scss/global.css'
 import { Provider } from 'react-redux'
-
+import store from '../state/store'
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     // <Provider> 
-      <Layout>
+      <Layout store={store}>
           <Component {...pageProps} />
       </Layout>
     // </Provider>

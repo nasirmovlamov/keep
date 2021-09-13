68689c57397721c77cc1377ac4daf9366a7fdb9a
nasirmovlamov <movlamovnasir2000@gmail.com>
2021-09-13 18:19:35 +0400

    images optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ['https://demo.abysshub.com'],
+  },
   reactStrictMode: true,
   eslint:{
     ignoreDuringBuilds: true,
